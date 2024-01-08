b9aecb45936aab5eb84d5aa9d308b7a0c245d5b8
asilbek <asilbekmirolimov@gmail.com>
2024-01-08 19:54:14 +0500

    changes

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from home.models import Vazifa
+
 # Register your models here.
+admin.site.register(Vazifa)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Vazifa(models.Model):
+    nomi = models.CharField(max_length=50, null=False)
+    tavsif = models.TextField(null=True)
+    oxirgi_muddat = models.DateField()
+
+    def __str__(self):
+        return f'{self.nomi}'
+
+    class Meta:
+        verbose_name = 'Vazifa'
+        verbose_name_plural = 'Vazifalar'
+
